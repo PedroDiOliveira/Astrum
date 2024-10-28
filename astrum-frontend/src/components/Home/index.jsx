c9b2c1880f89dee8190aaf06c1cdfd0b470a1e4e
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-10-28 09:33:22 -0300

    feat: implement stelar background image for the hole application

--- astrum-frontend/src/components/Home/index.jsx
+++ astrum-frontend/src/components/Home/index.jsx
@@ -2,22 +2,22 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-    const [nome, setNome] = useState('');  // Estado para armazenar o nome
+    const [nome, setNome] = useState(''); 
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let response = await axios.get("http://localhost:8080/bodie/planets/Jupiter");
-                setNome(response.data.nome);  // Atualiza o estado com o nome recebido
+                let response = await axios.get("http://localhost:8080/bodie/planets/Earth");
+                setNome(response.data.name); 
             } catch (error) {
                 console.error("Erro ao buscar o nome:", error);
             }
         };
 
         fetchData();
-    }, []);  // Executa apenas uma vez, quando o componente é montado
+    }, []);
 
     return (
-        <h1>{nome}</h1>  // Renderiza o nome corretamente
+        <h1>{nome}</h1>
     );
 }

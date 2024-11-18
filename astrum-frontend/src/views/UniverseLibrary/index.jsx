0d5b44412759cc0687d4dd90c8cdc1c37161d8e3
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-11-18 20:37:00 -0300

    refactor: make link card near from each other

--- astrum-frontend/src/views/UniverseLibrary/index.jsx
+++ astrum-frontend/src/views/UniverseLibrary/index.jsx
@@ -4,20 +4,17 @@ import AstrumLink from "../../components/ReusableComponents/AstrumLink";
 import "./styles.css";
 
 export default function UniverseLibrary() {
-  // Estado para armazenar os planetas retornados pela requisição
   const [planets, setPlanets] = useState([]);
 
-  // Função para buscar os dados
   async function getData() {
     try {
       const response = await axios.get("http://localhost:8080/planet/planets");
-      setPlanets(response.data); // Atualiza o estado com a lista de planetas
+      setPlanets(response.data);
     } catch (error) {
       console.error("Erro ao buscar dados dos planetas:", error);
     }
   }
 
-  // useEffect para chamar a função ao carregar o componente
   useEffect(() => {
     getData();
   }, []);

f4825e8d9e611eb9ac976e8283a1776d4fa2c5d2
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-12-02 21:53:46 -0300

    feat: trying to get url params

--- astrum-frontend/src/views/AstrumObject/index.jsx
+++ astrum-frontend/src/views/AstrumObject/index.jsx
@@ -3,8 +3,13 @@ import ObjectName from "../../components/AstrumObject/ObjectName"
 import ObjectHeader from "../../components/AstrumObject/ObjectHeader"
 import ObjectPhoto from "../../components/AstrumObject/ObjectPhoto"
 import fotoProvisoria from "../../assets/planets/mars.svg"
+import { useLocation } from "react-router-dom";
 
 export default function AstrumObject(){
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    console.log("ola" + queryParams)
+
     return(
         <div className="main-content">
             <ObjectHeader/>
